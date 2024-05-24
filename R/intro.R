7f2dc89ad6cffd907e4544f9aabc0b5297aa1732
Heru Handika <herubiolog@gmail.com>
2024-05-24 17:34:50 -0500

    Clean code.

--- R/intro.R
+++ R/intro.R
@@ -1,5 +1,35 @@
 # Test create variable
+pacman::p_load("here")
+pacman::p_load("readr")
+pacman::p_load("tidyr")
+pacman::p_load("ggplot2")
 
-myFirsVariable <- 'Testing R'
 
-testAddition <- 1 + 2
+
+
+# Assign path to variable
+mammals.path <- here::here("data", "IndonesianMammalVertNet.csv")
+
+# Load data frame
+mammals.df <- readr::read_csv(mammals.path)
+
+# Check column name
+colnames(mammals.df)
+
+mammals.length <- mammals.df |>
+  dplyr::select(order, family, genus, specificepithet, sex, lengthinmm, massing, minimumelevationinmeters)
+
+mammals.length <- mammals.length |>
+  tidyr::unite("species", genus:specificepithet, remove = FALSE)
+
+mammals.length |>
+  dplyr::filter(order != "Primates") |>
+  ggplot2::ggplot(aes(x = order, y = lengthinmm)) +
+  ggplot2::geom_point() +
+  ggplot2::theme_classic()
+
+mammals.length |>
+  dplyr::filter(order == "Rodentia") |>
+  ggplot2::ggplot(aes(x = minimumelevationinmeters, y = lengthinmm)) +
+  ggplot2::geom_point() +
+  ggplot2::theme_classic()
